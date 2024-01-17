0d33903d1abf655418d03acf5e22231191fdd331
Jonaxx8 <20r21a0543@mlrinstitutions.ac.in>
2024-01-17 22:31:41 +0530

    minor changes

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -3,7 +3,6 @@ import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
 import 'package:provider/provider.dart';
 import '../provider/movie_provider.dart';
 import '../widgets/movie_list.dart';
-import '../widgets/sort_dropdown.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -11,39 +10,6 @@ class MainPage extends StatefulWidget {
   @override
   State<MainPage> createState() => _MainPageState();
 }
-
-// class _MainPageState extends State<MainPage> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     Future.delayed(Duration.zero, () {
-//       Provider.of<MovieProvider>(context, listen: false).fetchTopRatedMovies();
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ChangeNotifierProvider<MovieProvider>.value(
-//       value: Provider.of<MovieProvider>(context),
-//       child: Scaffold(
-//         body: Builder(
-//           builder: (context) {
-//             final movieProvider = Provider.of<MovieProvider>(context);
-
-//             return LazyLoadScrollView(
-//               onEndOfPage: () {
-//                 if (!movieProvider.isLoading) {
-//                   context.read<MovieProvider>().fetchTopRatedMovies();
-//                 }
-//               },
-//               child: MovieList(movies: movieProvider.movies),
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 class _MainPageState extends State<MainPage> {
   @override
